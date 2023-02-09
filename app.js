08b3b839c88a999ad6770216f296a6b4cb0f54e9
bowersjuan <bowersjuan@gmail.com>
2023-02-09 12:50:10 -0500

    update app.js

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
+
 const app = express();
 
-app.use(cors);
+app.use(cors());
 app.use(morgan("tiny"));
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.json("App running...");
+  res.send("App running...");
 });
 
 app.get("*", (req, res) => {
