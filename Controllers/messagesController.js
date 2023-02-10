43c284c56da9294f97c5d74e85175f785bdc7098
bowersjuan <bowersjuan@gmail.com>
2023-02-10 15:26:50 -0500

    update backend app with full crud for messages

--- Controllers/messagesController.js
+++ Controllers/messagesController.js
@@ -1,13 +1,50 @@
 const express = require('express');
 const messages = express.Router();
 
-const { getAllMessages } = require('../Queries/messages');
+const { getAllMessages, getOneMessages, createMessage, deleteMessage, updateMessage } = require('../Queries/messages');
 
 messages.get('/', async (req, res) => {
   const allMessages = await getAllMessages();
   if (allMessages[0]) {
     res.status(200).json(allMessages);
-  } else res.status(500).json({ error: allMessages.message });
+  } else res.status(500).json({ error: "server error"});
 });
+
+messages.get('/:id', async (req, res) => {
+  const {id} = req.params;
+  const message = await getOneMessages(id)
+  if (message.id) {
+    res.status(200).json(message)
+  } else res.status(404).json({error: "message not found"})
+})
+
+messages.post('/', async (req, res) => {
+  try {
+    const newMessage = await createMessage(req.body)
+    res.status(200).json(newMessage)
+  } catch(error) {
+    res.status(500).json({error: "message not created"})
+  }
+})
+
+messages.delete('/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    const deletedMessage = await deleteMessage(id)
+    res.status(200).json(deletedMessage)
+  } catch (error) {
+    res.status(500).json({error: "id not found"})
+  }
+})
+
+messages.put('/:id', async (req, res) => {
+  const { id } = req.params; 
+  try {
+    const updatedMessage = await updateMessage(id, req.body)
+    res.status(200).json(updatedMessage)
+  } catch (error) {
+    res.status(500).json({error: "failed to update message"})
+  }
+})
 
 module.exports = messages;
--- Queries/messages.js
+++ Queries/messages.js
@@ -9,4 +9,41 @@ const getAllMessages = async () => {
   }
 };
 
-module.exports = { getAllMessages };
+const getOneMessages = async (id) => {
+  try {
+    const message = await db.one('SELECT * FROM messages WHERE id=$1', id);
+    return message;
+  } catch (error) {
+    return error;
+  }
+};
+
+const createMessage = async (message) => {
+  try {
+    const newMessage = await db.one('INSERT INTO messages (message, favorite, sender, receiver) VALUES ($1, $2, $3, $4) RETURNING *', [message.message, message.favorite, message.sender, message.receiver]);
+    return newMessage;
+  }
+  catch(error) {
+    return error
+  };
+}
+
+const deleteMessage = async (id) => {
+  try {
+    const deletedMessage = await db.one('DELETE FROM messages WHERE id=$1 RETURNING *', id)
+    return deletedMessage;
+  } catch (error) {
+    return error
+  }
+}
+
+const updateMessage = async (id, message) => {
+  try {
+    const updatedMessage = await db.one('UPDATE messages SET message=$1, favorite=$2, sender=$3, receiver=$4 WHERe id=$5 RETURNING *', [message.message, message.favorite, message.sender, message.receiver], id)
+    return updatedMessage;
+  } catch (error) {
+    return error
+  }
+}
+
+module.exports = { getAllMessages, getOneMessages, createMessage, deleteMessage, updateMessage };
