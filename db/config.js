483a4f19c50536f13b73e9e5c38c5dccf2e367ef
bowersjuan <bowersjuan@gmail.com>
2023-02-12 18:41:48 -0500

    edit config

---
+++ db/config.js
@@ -0,0 +1,16 @@
+// import and invoke pg-promise
+const pgp = require('pg-promise')();
+
+require('dotenv').config();
+
+const cn = {
+  host: process.env.PG_HOST,
+  port: process.env.PG_PORT,
+  database: process.env.PG_DATABASE,
+  user: process.env.PG_USER,
+  password: process.env.PG_PASSWORD,
+};
+
+const db = pgp(cn);
+
+module.exports = db;
