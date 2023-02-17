2dee85b3de32857bee7a76b1415f8d768486361d
bowersjuan <bowersjuan@gmail.com>
2023-02-16 21:34:19 -0500

    update schema.sql

--- db/seed.sql
+++ db/seed.sql
@@ -6,3 +6,8 @@ INSERT INTO messages (message, favorite) VALUES
 ('Tell bob234 to send me the check please', false),
 ('Can you send juan746 the check?', false),
 ('Okay I will', true);
+
+INSERT INTO users (username, password) VALUES 
+('Juan', 'dev'), ('Alex', 'dad');
+
+INSERT INTO users_messages (message_id, user_id) VALUES (1, 1), (5, 1), (3, 1), (2, 2), (4, 2);
