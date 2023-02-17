7ca21465432aec9a415cbd40ec050453010e7f95
bowersjuan <bowersjuan@gmail.com>
2023-02-17 03:33:42 -0500

    update seed.sql

--- db/seed.sql
+++ db/seed.sql
@@ -1,10 +1,10 @@
 \c messages_dev;
 
 INSERT INTO messages (message, favorite, sender_id) VALUES 
-('Hello how are you doing?', false, 2),
-('Good and you?', false, 1),
-('Tell Bob to send me the check please', false, 2),
-('Can you send the check now Juan?', false, 1),
-('Okay I will', true, 2);
+('Hello how are you doing?', true, 2),
+('Lets go to the park!', false, 1),
+('Tell Bob to send me the check please', true, 2),
+('Can you send the check now?', false, 1),
+('You are the best', true, 2);
 
 INSERT INTO users_messages (message_id, user_id) VALUES (1, 1), (5, 1), (3, 1), (2, 2), (4, 2);
