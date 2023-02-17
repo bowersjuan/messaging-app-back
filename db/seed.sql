8607a3130330d7352c912be8c98d31c179cb9140
bowersjuan <bowersjuan@gmail.com>
2023-02-16 22:11:01 -0500

    update seed.sql

--- db/seed.sql
+++ db/seed.sql
@@ -7,7 +7,4 @@ INSERT INTO messages (message, favorite) VALUES
 ('Can you send juan746 the check?', false),
 ('Okay I will', true);
 
-INSERT INTO users (username, password) VALUES 
-('Juan', 'dev'), ('Alex', 'dad');
-
 INSERT INTO users_messages (message_id, user_id) VALUES (1, 1), (5, 1), (3, 1), (2, 2), (4, 2);
