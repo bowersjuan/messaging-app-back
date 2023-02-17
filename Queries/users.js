6a65efecfdb380f256172ff4aded62616cf1885c
bowersjuan <bowersjuan@gmail.com>
2023-02-17 00:43:08 -0500

    update users query for get one message

--- Queries/users.js
+++ Queries/users.js
@@ -100,7 +100,7 @@ const getAllMessagesForUser = async (id) => {
 const getOneMessagesForUser = async (id) => {
   try {
     const messageByUser = await db.one(
-      'SELECT message, favorite, time_sent FROM messages WHERE messages.id = $1', id
+      'SELECT user_id, message, favorite, time_sent FROM messages WHERE messages.id = $1', id
     )
     return messageByUser;
   } catch(error) {
